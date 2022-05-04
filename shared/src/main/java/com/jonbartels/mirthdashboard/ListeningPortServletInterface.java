78ddb3247a850359fc72326b104d6481176049db
Jon Bartels <jonb@consultzen.com>
2022-05-04 08:44:56 -0400

    license and gitignore, traditional initial commit

---
+++ shared/src/main/java/com/jonbartels/mirthdashboard/ListeningPortServletInterface.java
@@ -0,0 +1,10 @@
+package com.jonbartels.mirthdashboard;/**
+ * You're viewing source code written by Zen Healthcare IT.
+ * https://consultzen.com
+ *
+ * Created by jon.bartels on 5/2/22 for the mirthpluginsample project using only 
+ *  locally sourced, organically grown, and humanely killed electrons.
+ * 
+ */
+public class ListeningPortServletInterface {
+}
